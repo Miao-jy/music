dfdb06324a383ba23ce5c9855301e143b228493b
miaojy <2306325634@qq.com>
2022-05-10 10:43:11 +0800

    发现界面，加入webView，抓包依然utf-8中文不显示

--- app/src/main/java/com/example/music/view/home/fragment/DiscoveryFragment.java
+++ app/src/main/java/com/example/music/view/home/fragment/DiscoveryFragment.java
@@ -1,9 +1,12 @@
 package com.example.music.view.home.fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -29,9 +32,21 @@ public class DiscoveryFragment extends Fragment {
         return discoveryFragment;
     }
 
+    private WebView discoveryWebView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_discory_layout, container, false);
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        discoveryWebView = view.findViewById(R.id.discovery_web_view);
+        discoveryWebView.getSettings().setJavaScriptEnabled(true);
+        discoveryWebView.setWebViewClient(new WebViewClient());
+        discoveryWebView.loadUrl("https://www.baidu.com");
     }
 }
